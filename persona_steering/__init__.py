3db432dce1214bc4b66a0e59361b25219cbdb77b
Girish Gupta <girish@girishgupta.com>
2026-04-06 17:21:10 -0700

    Removes comment

--- persona_steering/__init__.py
+++ persona_steering/__init__.py
@@ -8,7 +8,6 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-# Auto-load .env from project root (parent of this package)
 load_dotenv(Path(__file__).resolve().parent.parent / ".env")
 
 __version__ = "0.1.0"
